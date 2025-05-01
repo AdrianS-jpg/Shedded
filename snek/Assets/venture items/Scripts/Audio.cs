0812f47d1536ecb0f0ca2595d8d2b74bded77554
MUKILTEO\1720967 <1720967@mukilteo.wednet.edu>
2025-05-01 13:15:11 -0700

    Background music / Menu music added

---
+++ snek/Assets/venture items/Scripts/Audio.cs
@@ -0,0 +1,39 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Audio : MonoBehaviour
+{
+    private static Audio instance = null;
+
+
+    public static Audio Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+          
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+       
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Level 1")
+        {
+            Audio.instance.GetComponent<AudioSource>().Pause();
+        }
+    }
+
+}
